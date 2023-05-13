9c9270bcff1473c90178bcbbbb7f41da68040e64
alberto.contessini <alberto.contessini@accenture.com>
2023-05-13 14:57:29 +0200

    start

--- artist-market/src/app/app-routing.module.ts
+++ artist-market/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SearchArtistComponent } from './search-artist/search-artist.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'HomepageComponent',
+  },
+/*   {
+    path: 'search',
+    component: SearchArtistComponent,
+  }, */
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- artist-market/src/app/app.module.ts
+++ artist-market/src/app/app.module.ts
@@ -1,16 +1,45 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { HomepageComponent } from './homepage/homepage.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { SearchArtistComponent } from './search-artist/search-artist.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { MatInputModule } from '@angular/material/input';
+import {MatTableModule} from '@angular/material/table';
+import {MatSelectModule} from '@angular/material/select';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    HomepageComponent,
+    SearchArtistComponent,
+   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatCardModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatGridListModule,
+    MatInputModule,
+    MatTableModule,
+    MatSelectModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
